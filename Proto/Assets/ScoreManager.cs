0a8e531ea850fb984571f2858429128062f71954
Reskesen <Reskesen@gmail.com>
2015-11-03 15:58:44 +0100

    Added Score
    Added a score counter that starts on 1000 by default and then decreases
    1 for every 2 frames passed (relatively fast for testing purposes)

---
+++ Proto/Assets/ScoreManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour
+{
+	public static int score = 1000;
+//	private int time;
+	private bool done = false;
+	public int frames = 0;
+	Text text;
+	
+	
+	void Awake ()
+	{
+		text = GetComponent <Text> ();
+	}
+
+	void Update ()
+	{
+		frames = Time.frameCount;
+		if ((frames % 2) == 0 && !done) {
+			score--;
+			text.text = "Score: " + (score);
+			done = true;
+			} else if(!((frames % 2) == 0)) {
+				done = false;
+			}
+
+		//time = (int)Time.time;
+		//if ((time % 2) == 0 && !done) {
+		//	score--;
+		//	text.text = "Score: " + (score);
+		//	done = true;
+		//} else if(!((time % 2) == 0)) {
+		//	done = false;
+		//}
+	}
+}
