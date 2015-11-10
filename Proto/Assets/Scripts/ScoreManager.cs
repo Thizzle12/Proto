1f6f3fb1e776cce5e8095f70eccc0b6f7383d5fb
Reskesen <Reskesen@gmail.com>
2015-11-10 16:23:26 +0100

    Revert "Much cooler score and gameover text"
    This reverts commit 4c7e1bec6bd02f1dbb557d96034407db73449809.

--- Proto/Assets/Scripts/ScoreManager.cs
+++ Proto/Assets/Scripts/ScoreManager.cs
@@ -6,14 +6,12 @@ public class ScoreManager : MonoBehaviour
 {
 	public PlayerHealth playerHealth;
 	public GameObject player;
-	
+
 	private bool gameover = false;
 	public int score = 1000;
-	//	private int time;
+//	private int time;
 	private bool done = false;
 	public static int frames = 0;
-	public static int frames2 = 0;
-	public bool dead = false;
 	public int updateSpeed = 2;
 	Text text;
 	
@@ -24,48 +22,30 @@ public class ScoreManager : MonoBehaviour
 		player = GameObject.FindGameObjectWithTag ("Gamer");
 		playerHealth = player.GetComponent<PlayerHealth> ();
 	}
-	
+
 	void Update ()
 	{
-		if (score > 0 && playerHealth.currentHealth > 0 && !dead) {
-			Time.timeScale = 1.0f;
+		if (score > 0 && playerHealth.currentHealth > 0) {
 			frames = Time.frameCount;
 			if ((frames % updateSpeed) == 0 && !done) {
 				score--;
 				text.text = "Score: " + (score);
 				done = true;
 			} else if (!((frames % updateSpeed) == 0)) {
-				done = false;
-			}
-		} else if (!dead) {
-			frames2 = Time.frameCount;
-			dead = true;
+					done = false;
+				}
 		} else{
-			text.color = Color.red;
-			text.fontSize = 80;
-			text.alignment = TextAnchor.LowerCenter;
-			
 			text.text = "Game Over!";
-			Time.timeScale = 0.4f;
-			
-			if(frames2 + 300 <= Time.frameCount){
-				dead = false;
-				Die ();
-				
-			}
 		}
 	}
-	public void Die(){
-		Application.LoadLevel(Application.loadedLevel);
-	}
 	
-	//time = (int)Time.time;
-	//if ((time % 2) == 0 && !done) {
-	//	score--;
-	//	text.text = "Score: " + (score);
-	//	done = true;
-	//} else if(!((time % 2) == 0)) {
-	//	done = false;
-	//}
-	
+		//time = (int)Time.time;
+		//if ((time % 2) == 0 && !done) {
+		//	score--;
+		//	text.text = "Score: " + (score);
+		//	done = true;
+		//} else if(!((time % 2) == 0)) {
+		//	done = false;
+		//}
+
 }
