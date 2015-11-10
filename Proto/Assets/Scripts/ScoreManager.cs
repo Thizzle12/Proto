874f00e3f0c6e0b80889007192cde863df46c02d
Reskesen <Reskesen@gmail.com>
2015-11-10 16:27:39 +0100

    FIXED

--- Proto/Assets/Scripts/ScoreManager.cs
+++ Proto/Assets/Scripts/ScoreManager.cs
@@ -6,12 +6,14 @@ public class ScoreManager : MonoBehaviour
 {
 	public PlayerHealth playerHealth;
 	public GameObject player;
-
+	
 	private bool gameover = false;
 	public int score = 1000;
-//	private int time;
+	//	private int time;
 	private bool done = false;
 	public static int frames = 0;
+	public static int frames2 = 0;
+	public bool dead = false;
 	public int updateSpeed = 2;
 	Text text;
 	
@@ -22,30 +24,48 @@ public class ScoreManager : MonoBehaviour
 		player = GameObject.FindGameObjectWithTag ("Gamer");
 		playerHealth = player.GetComponent<PlayerHealth> ();
 	}
-
+	
 	void Update ()
 	{
-		if (score > 0 && playerHealth.currentHealth > 0) {
+		if (score > 0 && playerHealth.currentHealth > 0 && !dead) {
+			Time.timeScale = 1.0f;
 			frames = Time.frameCount;
 			if ((frames % updateSpeed) == 0 && !done) {
 				score--;
 				text.text = "Score: " + (score);
 				done = true;
 			} else if (!((frames % updateSpeed) == 0)) {
-					done = false;
-				}
+				done = false;
+			}
+		} else if (!dead) {
+			frames2 = Time.frameCount;
+			dead = true;
 		} else{
+			text.color = Color.red;
+			text.fontSize = 80;
+			text.alignment = TextAnchor.LowerCenter;
+			
 			text.text = "Game Over!";
+			Time.timeScale = 0.4f;
+			
+			if(frames2 + 300 <= Time.frameCount){
+				dead = false;
+				Die ();
+				
+			}
 		}
 	}
+	public void Die(){
+		Application.LoadLevel(Application.loadedLevel);
+	}
 	
-		//time = (int)Time.time;
-		//if ((time % 2) == 0 && !done) {
-		//	score--;
-		//	text.text = "Score: " + (score);
-		//	done = true;
-		//} else if(!((time % 2) == 0)) {
-		//	done = false;
-		//}
-
+	//time = (int)Time.time;
+	//if ((time % 2) == 0 && !done) {
+	//	score--;
+	//	text.text = "Score: " + (score);
+	//	done = true;
+	//} else if(!((time % 2) == 0)) {
+	//	done = false;
+	//}
+	
 }
