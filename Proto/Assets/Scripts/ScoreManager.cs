b4c0ff2b54c468b8d18f5326f2087227365f0a30
Reskesen <Reskesen@gmail.com>
2015-11-10 14:03:03 +0100

    Gameover feature added
    You are now gameover upon reaching 0 score or 0 health

--- Proto/Assets/Scripts/ScoreManager.cs
+++ Proto/Assets/Scripts/ScoreManager.cs
@@ -4,6 +4,10 @@ using System.Collections;
 
 public class ScoreManager : MonoBehaviour
 {
+	public PlayerHealth playerHealth;
+	public GameObject player;
+
+	private bool gameover = false;
 	public int score = 1000;
 //	private int time;
 	private bool done = false;
@@ -15,22 +19,25 @@ public class ScoreManager : MonoBehaviour
 	void Awake ()
 	{
 		text = GetComponent <Text> ();
+		player = GameObject.FindGameObjectWithTag ("Gamer");
+		playerHealth = player.GetComponent<PlayerHealth> ();
 	}
 
 	void Update ()
 	{
-		if (!(score <= 0)) {
+		if (score > 0 && playerHealth.currentHealth > 0) {
 			frames = Time.frameCount;
 			if ((frames % updateSpeed) == 0 && !done) {
 				score--;
 				text.text = "Score: " + (score);
 				done = true;
 			} else if (!((frames % updateSpeed) == 0)) {
-				done = false;
-			}
-		} else {
-			text.text = "Game over!";
+					done = false;
+				}
+		} else{
+			text.text = "Game Over!";
 		}
+	}
 	
 		//time = (int)Time.time;
 		//if ((time % 2) == 0 && !done) {
@@ -40,5 +47,5 @@ public class ScoreManager : MonoBehaviour
 		//} else if(!((time % 2) == 0)) {
 		//	done = false;
 		//}
-	}
+
 }
